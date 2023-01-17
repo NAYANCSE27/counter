6fb79fb78a148161fdee4916615e64dcbe9d0a23
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-01-17 18:34:58 +0600

    added full functional javascript file

--- script.js
+++ script.js
@@ -8,6 +8,27 @@ function calculateGrade() {
   if (marks >= 80 && marks <= 100) {
     document.getElementById("grade").innerText = "A+";
   }
+  else if (marks >= 70 && marks <= 79) {
+    document.getElementById("grade").innerText = "A";
+  }
+  else if (marks >= 60 && marks <= 69) {
+    document.getElementById("grade").innerText = "A-";
+  }
+  else if (marks >= 50 && marks <= 59) {
+    document.getElementById("grade").innerText = "B";
+  }
+  else if (marks >= 40 && marks <= 49) {
+    document.getElementById("grade").innerText = "C";
+  }
+  else if (marks >= 33 && marks <= 39) {
+    document.getElementById("grade").innerText = "D";
+  }
+  else if (marks >= 0 && marks <= 32) {
+    document.getElementById("grade").innerText = "You didn't pass with this marks.";
+  }
+  else{
+    document.getElementById("grade").innerText =  "Invalid marks."
+  }
 }
 
 setInitialValue();
