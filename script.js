25c807f6e9bcfd566a9bd3e428d07d8125d85cd5
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-01-17 18:26:13 +0600

    nayan grade calculator added in new branch

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-let count = 0;
-
-function setCount() {
-  document.getElementById("output").innerText = count;
+function setInitialValue() {
+  document.getElementById("marks-inp").value = 0;
 }
 
-function setInitialCount() {
-  setCount();
+function calculateGrade() {
+  const marks = parseInt(document.getElementById("marks-inp").value);
+  
+  if (marks >= 80 && marks <= 100) {
+    document.getElementById("grade").innerText = "A+";
+  }
 }
 
-function increment() {
-  count++;
-  setCount();
-}
-
-setInitialCount();
+setInitialValue();
