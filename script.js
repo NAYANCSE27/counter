c77edf2dc3eedfa9bf0dac6924256d1a1ef05293
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-01-19 15:11:42 +0600

    modify presentation of code

--- script.js
+++ script.js
@@ -7,26 +7,19 @@ function calculateGrade() {
   
   if (marks >= 80 && marks <= 100) {
     document.getElementById("grade").innerText = "You got A+";
-  }
-  else if (marks >= 70 && marks <= 79) {
+  }else if (marks >= 70 && marks <= 79) {
     document.getElementById("grade").innerText = "You got A";
-  }
-  else if (marks >= 60 && marks <= 69) {
+  }else if (marks >= 60 && marks <= 69) {
     document.getElementById("grade").innerText = "You got A-";
-  }
-  else if (marks >= 50 && marks <= 59) {
+  }else if (marks >= 50 && marks <= 59) {
     document.getElementById("grade").innerText = "You got B";
-  }
-  else if (marks >= 40 && marks <= 49) {
+  }else if (marks >= 40 && marks <= 49) {
     document.getElementById("grade").innerText = "You got C";
-  }
-  else if (marks >= 33 && marks <= 39) {
+  }else if (marks >= 33 && marks <= 39) {
     document.getElementById("grade").innerText = "You got D";
-  }
-  else if (marks >= 0 && marks <= 32) {
-    document.getElementById("grade").innerText = "You didn't pass with this marks.";
-  }
-  else{
+  }else if (marks >= 0 && marks <= 32) {
+    document.getElementById("grade").innerText = "You won't pass with this marks.";
+  }else{
     document.getElementById("grade").innerText =  "Invalid marks."
   }
 }
